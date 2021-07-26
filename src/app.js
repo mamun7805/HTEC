178c785e996ed2a9e04767b24b3f4bc8f52e2d3f
mamun4023 <mamun754023@gmail.com>
2021-07-27 01:15:44 +0600

    dashboard added

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
-
 import Dashboard from './containers/home';
 import Signin from './containers/signin';
 import Statistics from './containers/statistics';
@@ -21,7 +20,6 @@ function App(){
           </Switch>
           
       </Router>
-    
 
     </>
     )
--- src/components/dashboard/body/dashboard/index.js
+++ src/components/dashboard/body/dashboard/index.js
@@ -1,86 +1,122 @@
-import React from 'react';
+import React, {Component} from 'react';
+import mqtt from 'mqtt';
 import "./index.css";
 
 
-const data = [
+const machine_data = [
     {
         "name" : "Machine 01",
         "machine_no": "1",
-        "status" : 1
     },
     {
         "name" : "Machine 02",
         "machine_no": "2",
-        "status" : 0
     },
     {
         "name" : "Machine 03",
         "machine_no": "3",
-        "status" : 1
     },
     {
         "name" : "Machine 04",
         "machine_no": "4",
-        "status" : 1
     },
     {
         "name" : "Machine 05",
         "machine_no": "5",
-        "status" : 1
     },
     {
-        "name" : "Machine 05",
-        "machine_no": "5",
-        "status" : 1
+        "name" : "Machine 06",
+        "machine_no": "6",
     },
     {
-        "name" : "Machine 05",
-        "machine_no": "5",
-        "status" : 1
+        "name" : "Machine 07",
+        "machine_no": "7",
     },
     {
-        "name" : "Machine 05",
-        "machine_no": "5",
-        "status" : 1
+        "name" : "Machine 08",
+        "machine_no": "8",
     },
     {
-        "name" : "Machine 05",
-        "machine_no": "5",
-        "status" : 1
+        "name" : "Machine 09",
+        "machine_no": "9",
     },
     {
-        "name" : "Machine 05",
-        "machine_no": "5",
-        "status" : 1
+        "name" : "Machine 10",
+        "machine_no": "10",
     },
     {
-        "name" : "Machine 05",
-        "machine_no": "5",
-        "status" : 1
+        "name" : "Machine 11",
+        "machine_no": "11",
     }
 ]
 
 
+class Dashboard extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            machine : [],
+            topic : [],
+        }
+    }
 
-function Dashboard(){
+    componentDidMount() {
+        console.log("did mount.......................")
+        this.client = mqtt.connect('mqtt://127.0.0.1:8083');
+        this.client.options.username = 'sammy';
+        this.client.options.password = 'password';
+        console.log("client: ", this.client)
+        this.client.on("connect", () => {
+          console.log("connected");
+          this.client.subscribe("machine/+");
+        });
+        this.client.on('message', (topic, message) => {
+          this.handleJsonMessage(topic, message.toString());
+        })
+      }
 
-    return(
-        <>
+      handleJsonMessage = (topic, message) => {
+        // const temperatures = this.state.temperatures || []
+        // const humidities = this.state.humidities || []
+        // const lights = this.state.lights || []
+        // const pressures = this.state.pressures || []
+        // const time = Date.now();
+        // temperatures.push([time, json.temperature || 0])
+        // humidities.push([time, json.humidity || 0])
+        // lights.push([time, json.lux || 0])
+        // pressures.push([time, json.pressure || 0])
+        // this.setState({
+        //   data: { ...json },
+        //   temperatures,
+        //   humidities,
+        //   pressures,
+        //   lights
+        // })
+    
+        this.setState({
+          machine: machine_data,
+          topic : null
+        })
+      }
+
+      componentWillUnmount() {
+        if (this.client) {
+          this.client.end()
+        }
+      }
+    render(){
+        return(
+            <>
             <div className = "container machine-container">
-           {data.map(d=> 
-            
-                <div className = {d.status === 0? 'machine-off': "machine-on"}>
-                     {d.name}
-                     
-                </div>
-            
-            )} 
-             
+                 {
+                     machine_data.map(d => <div className="machine"> {d.name} </div>)
+                 }
             </div>
-
-
-        </>
-
-    )
+                    
+            </>
+        )
+    }
 }
-export default Dashboard;
+
+
+ export default Dashboard;
